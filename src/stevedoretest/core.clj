ada1ec9c7b730da7288a4781c7f49e2136be13e1
Mathieu Gauthron <mathieu@matlux.net>
2013-06-16 11:29:49 +0100

    straight ls displayed. Example 1

--- src/stevedoretest/core.clj
+++ src/stevedoretest/core.clj
@@ -1,39 +1,41 @@
 (ns stevedoretest.core
-(:require
-   [clojure.string :as string]
-   [clojure.test :refer [is testing]]
-   [clojure.tools.logging :as logging]
-   [pallet.common.filesystem :as filesystem]
-   [pallet.common.logging.logutils :as logutils]
-   [pallet.common.shell :as shell]
-   [pallet.common.string :refer [quoted]]
-   [pallet.script :as script]
-   [pallet.stevedore :refer :all]
-   [pallet.stevedore.bash :refer :all]
-   [pallet.stevedore.common]
-   [pallet.action.exec-script]
-   )
+
 
 
   )
 
+
+(require '[pallet.stevedore :refer [script with-script-language]])
+(require 'pallet.stevedore.bash) ;; for bash output
+
+(defn script-run [] (with-script-language :pallet.stevedore.bash/bash
+   (script
+    ("ls"))))
+
 ;(.bindRoot #'pallet.stevedore/*script-language* :pallet.stevedore.bash/bash)
+
+
+
+
+
+(defn -main
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!" (script-run)))
+
+
+
+(comment
 
 (script/defscript ls [& args])
 (script/defimpl ls :default [& args]
   (ls ~@args))
 
 
-
-
 (defn script-run [] (with-script-language :pallet.stevedore.bash/bash
    (script/with-script-context [:os-x]
-     (pallet.action.exec-script/exec-script (script ("ls -al ./"))))))
-
-
-
-(comment
-(with-script-language :pallet.stevedore.bash/bash
+     (pallet.action.exec-script/exec-script(script ("ls -al ./"))))))
+  (with-script-language :pallet.stevedore.bash/bash
   (pallet.action.exec-script/exec-script
    (script (ls "./"))))
 
@@ -69,8 +71,3 @@
 
 
   )
-
-(defn -main
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!" (script-run)))
